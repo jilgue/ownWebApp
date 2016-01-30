1f18a092032dc00c439b4b20a47ab0eacaa72b93
César M. Cristóbal <jilgue@gmail.com>
2016-01-30 12:56:49 +0100

    Fix nick and LoginController

--- app/js/app.js
+++ app/js/app.js
@@ -1,28 +1,28 @@
 'use strict';
 
-
 // Declare app level module which depends on filters, and services
-angular.module('ownApp', ['ngRoute']);
+angular.module('ownApp', ['ngRoute', 'loginModule']);
 
 angular.module('ownApp').config(function ($routeProvider, $locationProvider) {
     $routeProvider
 	.when('/login', {
 	    templateUrl: 'views/login.html',
 	    controller: 'LoginController',
-	    controllerAs: 'login'});
+	    controllerAs: 'login'
+	});
     $routeProvider.otherwise({ redirectTo: '/login' });
-    $locationProvider.html5Mode(true); //activate HTML5 Mode
+    //$locationProvider.html5Mode(true); //activate HTML5 Mode
 });
 
-angular.module('loginModule', []);
+var loginModule = angular.module('loginModule', []);
 
-angular.module('loginModule').controller('LoginController', ['$scope', '$http', function ($scope, $http) {
+loginModule.controller('LoginController', ['$scope', '$http', function ($scope, $http) {
 
     $scope.stLogin = function() {
 
 	$http.post('http://192.168.56.110/apirest/user/login.json',
 		   $.param({
-		       nick: $scope.login.nick,
+		       userName: $scope.login.userName,
 		       password: $scope.login.password
 		   }),{
 		       headers: { 'Content-Type': 'application/x-www-form-urlencoded' }}
